faf05995c1a88a75cbc20372b02732a27f3f8788
adil <adil@example.com>
2023-02-10 10:29:00 +0100

    dva

--- DrivingApp.Infrastructure/DatabaseContext.cs
+++ DrivingApp.Infrastructure/DatabaseContext.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using DrivingApp.Core;
+using DrivingApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DrivingApp.Infrastructure
 {
-    internal class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Condition> Conditions { get; set; }
+        public DbSet<Country>  Countries { get; set; }
+        public DbSet<MileStone> MileStones { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Ride> Rides { get; set; }
+        public DbSet<RideCondition> RideConditions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRide> UserRides { get; set; }
+        public DbSet<UserRideComment> UserRideComments { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
